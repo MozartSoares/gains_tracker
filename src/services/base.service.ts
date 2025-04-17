b3b4b77cbaa05d63955f837da534d4e2e6b592c8
MozartSoares <mozartarmstrong@gmail.com>
2025-04-17 03:49:12 -0300

    feat: add populate to findAll

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -4,48 +4,58 @@ export class BaseService<T> {
   constructor(protected model: ReturnModelType<new () => T>) {}
 
   async findAll(userId: string | null) {
-    return this.model.find({ deleted_at: { $exists: false }, 
-      $or: [
-      { userId },
-      { isPrivate: false }
-    ] }).exec();
+    return this.model
+      .find({
+        deleted_at: { $exists: false },
+        $or: [{ userId }, { isPrivate: false }],
+      })
+      .populate('userId', '-password -token')
+      .exec();
   }
 
   async findById(id: string, userId: string | null) {
-    return this.model.findOne({
-      _id: id,
-      deleted_at: { $exists: false },
-      $or: [{ userId }, { isPrivate: false }],
-    }).exec();
+    return this.model
+      .findOne({
+        _id: id,
+        deleted_at: { $exists: false },
+        $or: [{ userId }, { isPrivate: false }],
+      })
+      .exec();
   }
 
-  async create(data: Partial<T>,userId:string) {
-    const entity = new this.model({...data,userId});
+  async create(data: Partial<T>, userId: string) {
+    const entity = new this.model({ ...data, userId });
     return entity.save();
   }
 
-  async update(id: string, data: Partial<T>,userId:string) {
-    return this.model.findOneAndUpdate(
-      { _id: id, deleted_at: { $exists: false }, userId },
-      { ...data, updated_at: new Date() },
-      { new: true }
-    ).exec();
+  async update(id: string, data: Partial<T>, userId: string) {
+    return this.model
+      .findOneAndUpdate(
+        { _id: id, deleted_at: { $exists: false }, userId },
+        { ...data, updated_at: new Date() },
+        { new: true },
+      )
+      .exec();
   }
 
-  async delete(id: string,userId:string): Promise<boolean> {
-    const result = await this.model.findOneAndUpdate(
-      { _id: id, deleted_at: { $exists: false }, userId },
-      { deleted_at: new Date() },
-      { new: true }
-    ).exec();
+  async delete(id: string, userId: string): Promise<boolean> {
+    const result = await this.model
+      .findOneAndUpdate(
+        { _id: id, deleted_at: { $exists: false }, userId },
+        { deleted_at: new Date() },
+        { new: true },
+      )
+      .exec();
     return !!result;
   }
 
   /**Internal route, not accessible to the public */
   async hardDelete(id: string): Promise<boolean> {
-    const result = await this.model.findOneAndDelete({ 
-      _id: id,  
-    }).exec();
+    const result = await this.model
+      .findOneAndDelete({
+        _id: id,
+      })
+      .exec();
     return !!result;
   }
 }
